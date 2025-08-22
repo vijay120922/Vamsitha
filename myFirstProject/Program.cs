ec3b3c488bbf189aa94ce2688ce61d928b05327a
vijay190922 <n200305@rguktn.ac.in>
2025-08-22 17:30:55 +0530

    first commit

---
+++ myFirstProject/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myFirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int a = 0;
+            int b = 1;
+            Console.Write($"{a},{b}");
+            for (int i = 0; i < n; i++)
+            {
+                int c = a + b;
+                Console.Write(","+c);
+                a = b;
+                b=c;
+            }
+            Console.WriteLine();
+            for (int i = 0; i < n; i++)
+            {
+                int c = a - b;
+                Console.Write("," + c);
+                a = b;
+                b = c;
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
